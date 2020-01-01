323a58a8d8ac5a8edfdb1058a3a9b4068084a7bb
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2019-12-31 17:46:49 -0800

    Add files via upload
    1. I want to satisfy this user test: Pressing input One and then "=" will make input Two "0", but I can't do that with the current logic of the methods
    
    When you come back: Work on lines 268-280

--- arithmetic_methods.java
+++ arithmetic_methods.java
@@ -6,14 +6,13 @@ public class arithmetic_methods {
 	
 	//This value will change depending on what calculation is being performed
 	private static double results;															//What will displayed after calculations are complete
-	public static String stringResults;														//What will be displayed in the calculator_gui
 	public static double inputOne;															//Stores input one (Note: inputOne will end up containing the result if an arithmetic symbol was pushed instead of the equals sign)
 	public static double inputTwo;															//Stores input two
 	public static String arithSymbol;														//Stores the Arithmetic Symbol
 
 
 	//Arithmetic Method
-	public String equals() {
+	public static String equals() {
 		if(calculator_gui.secondInput) {													//Only calculate when the second input has been submitted
 			switch(arithSymbol) {
 				case "+":	//Adding
